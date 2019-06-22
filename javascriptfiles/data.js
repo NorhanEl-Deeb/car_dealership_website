efc348eb3b8f5987013c812a924fb2df8e0609f0
norhangit <norhan@yahoo.com>
2019-06-22 22:41:57 +0200

    final version

---
+++ javascriptfiles/data.js
@@ -0,0 +1,11 @@
+/* ------------------------ #### This code is written by Norhan El-Deeb @2019 #### ------------------- */
+
+
+/* -------------------------------------------Preloader section-------------------------------------- */
+window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'))
+
+
+
+
+
+
